655fc8e82b0e77343bc84d91038cc73dbd0e71a4
Matthew Olker <molker@i145>
2017-02-28 23:41:59 -0500

    Initial Commit

---
+++ quiz/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  quiz
+//
+//  Created by _____________ on ____________.
+//  Copyright © 2016 CSC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = ["From what is cognac is made?" ,
+                               "What is 7+7?",
+                               "What is the capital of Vermont"]
+    
+    let answers: [String] = ["Grapes","14","Montpelier"]
+    
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+           currentQuestionIndex = 0
+        }
+        questionLabel.text = questions[currentQuestionIndex]
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+
+}
+
