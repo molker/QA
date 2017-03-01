06a703ed8e656435ba7835cf69c288d903bee1fd
Matthew Olker <molker@i145>
2017-03-01 00:33:38 -0500

    basic structure set up

---
+++ quiz/QAInputViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WelcomeViewController.swift
+//  QA
+//
+//  Created by Matthew Olker on 2/28/17.
+//  Copyright © 2017 CSC. All rights reserved.
+//
+
+import UIKit
+
+class QAInputViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
